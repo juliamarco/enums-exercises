e8293e97d9faacb04103c6560181d580efaed258
Julia Marco <marcootj@gmail.com>
2018-12-16 10:33:48 -0700

    Adds sort_by and count

--- exercises/count_pattern_test.rb
+++ exercises/count_pattern_test.rb
@@ -17,51 +17,83 @@ class CountPatternTest < Minitest::Test
     numbers = [9, 18, 12, 17, 1, 3, 99]
     tally = 0
     numbers.each do |number|
-      # Your code goes here
+      if number > 17
+        tally += 1
+      end
     end
     assert_equal 2, tally
   end
 
   def test_count_words_that_are_uppercase
-    skip
+
     words = ["trousers", "SOCKS", "sweater", "Cap", "SHOE", "TIE"]
     tally = 0
-    # Your code goes here
+    words.each do |word|
+      if word.upcase == word
+        tally += 1
+      end
+    end
     assert_equal 3, tally
   end
 
   def test_count_words_ending_in_ing
-    skip
+
     words = ["thought", "brake", "shin", "juice", "trash"]
-    # Your code goes here
+    tally = 0
+    words.each do |word|
+      if word[-3..-1] == "ing"
+        tally += 1
+      end
+    end
     assert_equal 0, tally
   end
 
   def test_count_even_numbers
-    skip
+
     numbers = [9, 2, 1, 3, 18, 39, 71, 4, 6]
-    # Your code goes here
+    tally = 0
+    numbers.each do |number|
+      if number.even?
+        tally += 1
+      end
+    end
     assert_equal 4, tally
   end
 
   def test_count_multiples_of_5
-    skip
+
     numbers = [2, 5, 19, 25, 35, 67]
-    # Your code goes here
+    tally = 0
+    numbers.each do |number|
+      if number % 5 == 0
+        tally += 1
+      end
+    end
     assert_equal 3, tally
   end
 
   def test_count_round_prices
-    skip
+
     prices = [1.0, 3.9, 5.99, 18.5, 20.0]
-    # Your code goes here
+    tally = 0
+    prices.each do |price|
+      # require 'pry'; binding.pry
+      if price == price.round
+        tally += 1
+      end
+    end
     assert_equal 2, tally
   end
 
   def test_count_four_letter_words
-    skip
+
     words = ["bake", "bark", "corn", "apple", "wart", "bird", "umbrella", "fart"]
-    # Your code goes here
+    tally = 0
+    words.each do |word|
+      if word.length == 4
+        tally += 1
+      end
+    end
     assert_equal 6, tally
   end
 
